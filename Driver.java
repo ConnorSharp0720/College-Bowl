e0b3d306f9a1543750b720421e9b88dddeb3a5ff
Connor Sharp <cjsharp99@gmail.com>
2019-09-04 13:53:46 -0500

    completed the main functionality of the Driver abstract later

--- Driver.java
+++ Driver.java
@@ -12,11 +12,11 @@ public class Driver
         int answer;
         
         System.out.println("Please choose from the following menu of choices: \n" + 
-                "\n 1. Enter Teams \n 2. Enter Scores \n 3. Quit \n What is your choice?");
+                "\n 1. Enter Teams \n 2. Enter Scores \n 3. Top Scores \n 3. Quit \n What is your choice?");
         
         answer = input.nextInt();
         input.nextLine();
-        while(answer != 3)
+        while(answer != 4)
         {
             if(answer == 1)
             {
@@ -30,6 +30,7 @@ public class Driver
                 {
                     tempTeam = new Team(Teams.size() + 1, check);
                     Teams.add(tempTeam);
+                    System.out.println(check + " is team Number: " + Teams.size());
                    
                     System.out.println("Enter A Team Name or enter 0 to quit");
                     check = input.nextLine();
@@ -51,13 +52,35 @@ public class Driver
                     score = input.nextInt();
                     Teams.get(num - 1).addScore(score);
                    
-                    System.out.println("Enter A Team Name or enter 0 to quit");
+                    System.out.println("Enter A Team Number or enter 0 to quit");
                     num = input.nextInt();
                 }
             }
            
+            if(answer == 3)
+            {
+                Team top1 = new Team(0, "random");
+                Team top2 = new Team(0, "random");
+                Team top3 = new Team(0, "random");
+                
+                System.out.println("The Top 3 Teams are:");
+                
+                for(Team teamCompare: Teams)
+                {
+                    if(teamCompare.getScore() > top1.getScore())
+                    {
+                        top3 = top2;
+                        top2 = top1;
+                        top1 = teamCompare;
+                        
+                    }
+                }
+                
+                System.out.println(top1 + "\n" + top2 + "\n" + top3 + "\n");
+            }
+            
             System.out.println("Please choose from the following menu of choices: \n" + 
-                    "\n 1. Enter Teams \n 2. Enter Scores \n 3. Quit \n What is your choice?");
+                    "\n 1. Enter Teams \n 2. Enter Scores \n 3. Top Scores \n 3. Quit \n What is your choice?");
             
             answer = input.nextInt();
             input.nextLine();
@@ -67,6 +90,7 @@ public class Driver
         {
             System.out.println(teamPrint);
         }
+        input.close();
     }
 
 }
